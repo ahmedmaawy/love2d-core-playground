fe02170f7ad0982f59cb49bf95056bae58d38421
Ahmed Maawy <ultimateprogramer@gmail.com>
2020-10-04 10:29:05 +0300

    Move Module list to Init instead

--- sample/sample-module-test.lua
+++ sample/sample-module-test.lua
@@ -5,17 +5,18 @@ Demonstrate the use of the sample library
 
 function love.load()
     love.graphics.setFont(love.graphics.newFont(11))
+
+    -- Will display all the modules loaded onto the console.
+    for key,value in pairs(love) do
+        print("found member " .. key);
+    end
 end
 
 function love.draw()
     -- Gets the x- and y-position of the mouse.
     local x = 50
     local y = 50
-
-    -- Will display all the modules loaded onto the console.
-    for key,value in pairs(love) do
-        print("found member " .. key);
-    end
+    
 
     -- Will use the `doSum` function for the `love.sample` module
     local sum = love.sample.doSum(2, 4)
