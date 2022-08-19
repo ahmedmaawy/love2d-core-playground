4a0b59fa33e888b9f18b087568c3381708c5caef
Ahmed Maawy <ultimateprogramer@gmail.com>
2022-08-19 15:45:31 +0400

    Clean up the code to make it more efficient

--- sample/sample-module-test.lua
+++ sample/sample-module-test.lua
@@ -3,6 +3,8 @@
 Demonstrate the use of the sample library
 ]]
 
+local sum
+
 function love.load()
     love.graphics.setFont(love.graphics.newFont(11))
 
@@ -10,17 +12,12 @@ function love.load()
     for key,value in pairs(love) do
         print("found member " .. key);
     end
+    
+    -- Will use the `doSum` function for the `love.sample` module
+    sum = love.sample.doSum(2, 4)
 end
 
 function love.draw()
-    -- Gets the x- and y-position of the mouse.
-    local x = 50
-    local y = 50
-    
-
-    -- Will use the `doSum` function for the `love.sample` module
-    local sum = love.sample.doSum(2, 4)
-
     -- Draws the position on screen.
     love.graphics.print("The sum is " .. sum, 50, 50)
 end
